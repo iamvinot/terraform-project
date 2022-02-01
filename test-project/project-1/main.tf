8022bda825bedddfedcd89a239fd1b34b5dc55dd
Vinoth Mohan <vinothmohanc@gmail.com>
2022-02-01 07:22:34 +0530

    refactor

---
+++ test-project/project-1/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-south-1"
+}
+
+resource "aws_instance" "app_server"{
+  ami           = var.ami
+  instance_type = var.instance_type
+  tags = {
+    Name = "TerraInstance"
+  }
+}
---
+++ test-project/project-1/variables.tf
@@ -0,0 +1,8 @@
+variable "ami" {
+    type = string
+    default = "ami-0c6615d1e95c98aca"
+}
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+}
