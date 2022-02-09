eee42f040ff6a37bbefc51cad19ff465300f48d2
Vinoth Mohan <vinothmohanc@gmail.com>
2022-02-09 19:12:23 +0530

    add main

---
+++ project-1/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-south-1"
+}
+
+resource "aws_instance" "app_server"{
+  ami           = "ami-0c6615d1e95c98aca"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
