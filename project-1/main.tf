2fbfb051e1a294d3e792ce5571af9f2f66913b8e
Vinoth Mohan <vinothmohanc@gmail.com>
2022-02-10 11:55:51 +0530

    Test

--- project-1/main.tf
+++ project-1/main.tf
@@ -15,9 +15,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server"{
-  ami           = "ami-0c6615d1e95c98aca"
-  instance_type = "t2.micro"
+  ami           = var.ami
+  instance_type = var.instance_type
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "TerraInstance"
   }
 }
